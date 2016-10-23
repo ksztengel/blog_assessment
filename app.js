06b6f1c999e8166340cc6aff978b82ad0df71dec
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-23 08:41:13 -0600

    trying to get cookies to work heroku

--- app.js
+++ app.js
@@ -9,18 +9,19 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const app = express();
 const knex = require('./db/knex');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const configAuth = require('./public/config/auth');
 
+const app = express();
+
+app.use(passport.initialize());
 app.use(cookieSession({
     name: 'blogpositive',
     secret: process.env.SESSION_SECRET,
     secureProxy: app.get('env') === 'production'
 }));
-app.use(passport.initialize());
 
 passport.use(new FacebookStrategy({
         clientID: configAuth.clientID,
@@ -61,20 +62,6 @@ switch (app.get('env')) {
   default:
 }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(bodyParser.json())
-
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 const routes = require('./routes/index');
 const blog = require('./routes/blog');
 const users = require('./routes/users');
@@ -83,6 +70,19 @@ const comments = require('./routes/comments');
 const login = require('./routes/login');
 const logout = require('./routes/logout');
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(morgan('dev'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/blog', blog);
@@ -91,6 +91,7 @@ app.use('/posts', posts);
 app.use('/comments', comments);
 app.use('/login', login);
 app.use('/logout', logout);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
